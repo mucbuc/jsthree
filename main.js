654fd72580a1dfdd1b37d3e7515684f241c182a6
markymark <smallitle@hotmail.com>
2013-09-13 03:59:38 -0700

    fix typo

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 
 exports.exec = require( './lib/exec' ).exec;
 exports.walk = require( './lib/walk' ).walk;
-exports.Network = require( './lib/Network' ).NetWork;
+exports.Network = require( './lib/Network' ).Network;
