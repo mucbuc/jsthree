e611e9432af8f18418ea3996e7178acbecf2e43f
markymark <smallitle@hotmail.com>
2013-09-21 18:46:24 -0700

    fix cwd argument, test uses dummy file

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 var assert = require( 'assert' )
-  , Processor = require( './../lib/processor' ).Processor;
+  , Processor = require( './../lib/processor' ).Processor
+  , path = require( 'path' );
 
 assert( typeof Processor != 'undefined' );
 
@@ -10,17 +11,19 @@ checkOut();
 function checkOut() {
 
 	var p = new Processor()
-	  , result = '';
+	  , result = ''
+	  , cwd = path.join( __dirname, 'sample' );
 
 	p.on( 'out', function( data ) { 
 		result += data; 
 	} );
 
 	p.on( 'exit', function() {
-		assert( result.trim() == 'test.js' );
+		console.log( result );
+		assert( result.trim() == 'test_dummy.txt' );
 		console.log( 'checkOut passed' );
 	} );
 
-	p.onTickEmit( 'exec', 'ls', __dirname );
+	p.onTickEmit( 'exec', 'ls', cwd );
 	p.tick();
 }
