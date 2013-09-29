330059ec58afbe8969704f2a4246f7f3ffe46478
markymark <smallitle@hotmail.com>
2013-09-29 11:47:42 -0700

    dirscout test

--- test/test_dirscout.js
+++ test/test_dirscout.js
@@ -1,6 +1,29 @@
 #!/usr/bin/env node
 
 var assert = require( 'assert' )
+  , events = require( 'events' )
   , DirScout = require( './../lib/dirscout' ).DirScout;
 
 assert( DirScout );
+
+testGetList();
+
+function testGetList() {
+	var emitter = new events.EventEmitter()
+	  , passed = false;
+	
+	emitter.on( 'ls', function( data ) {
+	
+		if (data.toString() === [ 'test_dummy.txt' ].toString() ) {
+			passed = true;
+			console.log( 'dirscout test passed' );
+		}
+	} );
+
+	process.on( 'exit', function() { 
+		assert( passed );
+	} );
+
+	DirScout.getList( emitter, 'sample' );
+}
+
