9ede03bdfb4774891220bc0da61334008d35131f
markymark <smallitle@hotmail.com>
2013-09-29 19:30:34 -0700

    fixed path

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ exports.exec = require( './lib/exec' ).exec;
 exports.walk = require( './lib/walk' ).walk;
 exports.Network = require( './lib/network' ).Network;
 exports.Processor = require( './lib/processor' ).Processor;
-exports.DirScout = require( '.lib/dirscout' ).DirScout;
+exports.DirScout = require( './lib/dirscout' ).DirScout;
