59576b01621a6d406d3d1a233c7ffd373c83d859
markymark <smallitle@hotmail.com>
2013-11-18 15:52:06 -0800

    cleanup tester

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ exports.walk = require( './lib/walk' ).walk;
 exports.Network = require( './lib/network' ).Network;
 exports.Processor = require( './lib/processor' ).Processor;
 exports.DirScout = require( './lib/dirscout' ).DirScout;
-exports.Tester = require( './lib/tester' ).Tester;
+exports.Test = require( './lib/test' ).Test;
