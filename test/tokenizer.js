deb0a5ebfb0fdaadcdb95bf36b6c28fa7f5698f6
markymark <smallitle@hotmail.com>
2014-05-31 18:00:02 -0700

    kinda working

--- test/tokenizer.js
+++ test/tokenizer.js
@@ -13,13 +13,21 @@ function checkTokenizer() {
   var emitter = new TestEmitter();
 
   test( testDummyCode );
+  test( testTwoMatches );
+
+  function testTwoMatches( emitter, tokenizer ) {
+    tokenizer.match( ';', 'statement' );
+    tokenizer.match( '\\(', 'open' );
+    emitter.expect( 'open' );
+    emitter.expect( 'statement' );
+    tokenizer.process( '(;' );
+  }
 
   function testDummyCode( emitter, tokenizer ) {
     tokenizer.match( ';', 'statement' );
     emitter.expect( 'statement' );
     tokenizer.process( ';' );
   }
-
 
   function test( f ) {
     process.on( 'exit', function() {
