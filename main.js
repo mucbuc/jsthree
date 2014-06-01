ca89eaa1ebfd19012ecf107c32e6e44f584e530d
markymark <smallitle@hotmail.com>
2014-05-31 17:32:26 -0700

    dummy tokenizer passes

--- main.js
+++ main.js
@@ -4,3 +4,4 @@ exports.Processor = require( './lib/processor' ).Processor;
 exports.DirScout = require( './lib/dirscout' ).DirScout;
 exports.Test = require( './lib/test' ).Test;
 exports.Parser = require( './lib/parser' ).Parser;
+exports.Tokenizer = require( './lib/tokenizer' ).Tokenizer;
