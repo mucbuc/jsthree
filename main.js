d3608918c275c5f9d7814538e1db9c9429362171
markymark <smallitle@hotmail.com>
2014-11-30 09:52:53 -0800

    fix missing dependency"

--- main.js
+++ main.js
@@ -3,5 +3,4 @@ exports.Network = require( './lib/network' ).Network;
 exports.Processor = require( './lib/processor' ).Processor;
 exports.DirScout = require( './lib/dirscout' ).DirScout;
 exports.Test = require( './lib/test' ).Test;
-exports.Parser = require( './lib/parser' ).Parser;
-exports.Tokenizer = require( './lib/tokenizer' ).Tokenizer;
+
--- test/test.js
+++ test/test.js
@@ -4,4 +4,3 @@ var cp = require( 'child_process' );
 
 cp.fork( 'test_processor' );
 cp.fork( 'test_dirscout' );
-cp.fork( 'parser' );
